d60d1dece76b5c522d75078f74a4a0b4b4ebe7f2
Tyler Menezes <tylermenezes@gmail.com>
2018-02-01 12:24:52 -0800

    Update index language

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'by-batch'          => "Batches",
-    'by-region'         => "Regions",
+    'by-batch'          => "Seasons",
+    'by-region'         => "Locations",
     'my-events'         => "My Events",
     'current'           => "Happening Now",
     'title'             => "Click to see what fun projects students worked on:",
